1a6b9da7683627e794a66e6485adb39d12952a6c
George Andrew Phineas Coon <phineas@Georges-MacBook-Pro.local>
2020-01-27 14:50:41 -0700

    classes done

--- classes.js
+++ classes.js
@@ -30,7 +30,18 @@
 */
 
 //Code Here
+class Employee{
+  constructor(first_name, last_name, email, age){
+    this.first_name = first_name
+    this.last_name = last_name
+    this.email = email
+    this.age = age
+  }
 
+  makeWidget(){
+    return `${this.first_name} ${this.last_name} Widget`
+  }
+}
 
 ////////// PROBLEM 2 //////////
 
@@ -48,13 +59,27 @@
 */
 
 //Code Here
+class Manager extends Employee{
+  constructor(first_name, last_name, email, age){
+    super(first_name, last_name, email, age)
+    this.reports = []
+  }
 
+  hire(employee){
+    return this.reports.push(employee)
+  }
+
+  fire(index){
+    return this.reports.splice(index, 1)
+  }
+}
 
 ////////// PROBLEM 3 //////////
 
 /*
   Managers for Widget Co. get promoted when they get more employees, and get a bonus when they fire employees.
-  create a class ProgressiveManager that extends Manager.  A Progressive Manager has all of the same properties as a manager with the following additional properties:
+  create a class ProgressiveManager that extends Manager.  
+  A Progressive Manager has all of the same properties as a manager with the following additional properties:
     - title - default 'Not a manager'
     - bonus - default 0
 
@@ -72,7 +97,49 @@
 */
 
 //Code Here
+class ProgressiveManager extends Manager {
+  constructor(first_name, last_name, email, age){
+    super(first_name, last_name, email, age)
+    this.title = 'Not a manager'
+    this.bonus = 0
+  }
+  hire(employee){
+    this.reports.push(employee)
+    if(this.reports.length === 0){
+      this.title = 'Not a manager'
+    }else if(this.reports.length >= 1 && this.reports.length <= 3){
+      this.title = 'Barely Manager'
+    }else if(this.reports.length >= 4 && this.reports.length <= 10){
+      this.title = 'Mostly Manager'
+    }else if(this.reports.length >= 11 && this.reports.length <= 50){
+      this.title = 'Manager'
+    }else if(this.reports.length >= 51 && this.reports.length <= 100){
+      this.title = 'Manager Plus'
+    }else {
+      this.title = 'Bestest Manager'
+    }
+    return this.reports
+  }
 
+  fire(index){
+    this.reports.splice(index, 1)
+    this.bonus += 100
+    if(this.reports.length === 0){
+      this.title = 'Not a manager'
+    }else if(this.reports.length >= 1 && this.reports.length <= 3){
+      this.title = 'Barely Manager'
+    }else if(this.reports.length >= 4 && this.reports.length <= 10){
+      this.title = 'Mostly Manager'
+    }else if(this.reports.length >= 11 && this.reports.length <= 50){
+      this.title = 'Manager'
+    }else if(this.reports.length >= 51 && this.reports.length <= 100){
+      this.title = 'Manager Plus'
+    }else {
+      this.title = 'Bestest Manager'
+    }
+    return this.reports
+  }
+}
 
 
 ////////// PROBLEM 4 - Black Diamond //////////
@@ -99,5 +166,28 @@
 */
 
 //Code Here
+class Machine {
+  constructor(){
+    this.widgets_made_count = 0
+    this.wear_and_tear_count = 0
+    this.needs_reboot = false
+  }
+  
+  makeWidgets(number){
+    this.widgets_made_count += number
+   
+    console.log(this.wear_and_tear_count += number/50)
+  }
 
+  fixMachine(){
+    this.needs_reboot = true
+  }
+
+  reboot(){
+    return () =>{
+      this.wear_and_tear_count -= 10
+      this.needs_reboot = false
+    }
+  }
+}
 
